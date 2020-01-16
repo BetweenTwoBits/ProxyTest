b08dc9c5b8d048be6be13ecd653b1cf4b91c937e
Brandon Davis <brandon_davis5@comcast.com>
2020-01-16 10:31:39 -0700

    add basic okhttp impl

--- app/src/main/java/com/betweentwobits/proxytest/MainActivity.kt
+++ app/src/main/java/com/betweentwobits/proxytest/MainActivity.kt
@@ -1,12 +1,49 @@
 package com.betweentwobits.proxytest
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+    private val client = OkHttpClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        requestFile()
+    }
+
+    private fun requestFile() {
+        val request = Request.Builder()
+            .url("https://www.test.com/list.json")
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) {
+                        throw IOException("Unexpected response code: $response")
+                    }
+
+                    for ((name, value) in response.headers) {
+                        Log.d("Response Headers", "$name: $value")
+                    }
+
+                    Log.d("Response Body", response.body?.string().orEmpty())
+                }
+
+            }
+        })
     }
 }
